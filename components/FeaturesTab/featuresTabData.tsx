6c733a311ceb8bf086f99d789d07df77f6b5033f
dianfer <dainfer@dbjsystem.com>
2024-03-07 11:35:22 -0500

    terminando pagina de servicios faltan imagenes

--- components/FeaturesTab/featuresTabData.tsx
+++ components/FeaturesTab/featuresTabData.tsx
@@ -3,28 +3,34 @@ import { FeatureTab } from "@/types/featureTab";
 const featuresTabData: FeatureTab[] = [
   {
     id: "tabOne",
-    title: "Solid Has Neat & Clean User Interface.",
-    desc1: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies lacus non fermentum ultrices. Fusce consectetur le.`,
-    desc2: `    Nam id eleifend dui, id iaculis purus. Etiam lobortis neque nec finibus sagittis. Nulla ligula nunc egestas ut.`,
+    title: "Soluciones de Software Hechas a Tu Medida.",
+    desc1: `Transformamos tus ideas en realidades digitales. Utilizando Python, una herramienta poderosa y versátil, 
+    creamos sistemas personalizados que se adaptan perfectamente a las necesidades de tu negocio. `,
+    desc2: `Desde sitios web 
+    hasta aplicaciones complejas, construimos plataformas intuitivas y fáciles de usar que impulsan tu crecimiento.`,
     image: "/images/features/features-light-01.png",
-    imageDark: "/images/features/features-dark-01.svg",
+    imageDark: "/images/features/features-dark-01.svg"
   },
   {
     id: "tabTwo",
-    title: "Ready to Use Pages You Need for a SaaS Business.",
-    desc1: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies lacus non fermentum ultrices. Fusce consectetur le.`,
-    desc2: `    Nam id eleifend dui, id iaculis purus. Etiam lobortis neque nec finibus sagittis. Nulla ligula nunc egestas ut.`,
+    title: "Tableros y Control al Alcance de Tu Mano.",
+    desc1: `Toma el control de tus operaciones con dashboards claros y detallados. Ofrecemos sistemas de seguimiento 
+    y análisis que te permiten observar el rendimiento y las métricas clave de tu empresa en tiempo real.`,
+    desc2: `A través de herramientas avanzadas, simplificamos la monitorización para que puedas tomar decisiones 
+    informadas con facilidad.`,
     image: "/images/features/features-light-01.png",
-    imageDark: "/images/features/features-dark-01.svg",
+    imageDark: "/images/features/features-dark-01.svg"
   },
   {
     id: "tabThree",
-    title: "Functional Blog, DB, Auth and Many More",
-    desc1: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies lacus non fermentum ultrices. Fusce consectetur le.`,
-    desc2: `Nam id eleifend dui, id iaculis purus. Etiam lobortis neque nec finibus sagittis. Nulla ligula nunc egestas ut.`,
+    title: "Impulsa tu Negocio con Asesoría Experta",
+    desc1: `Navega el mundo de la tecnología con confianza. Brindamos asesorías que te ayudarán a entender y aprovechar 
+    al máximo las herramientas digitales para tu empresa. `,
+    desc2: `Desde estrategias de implementación hasta la optimización de procesos, te acompañamos en cada paso para
+     garantizar tu éxito en la era digital.`,
     image: "/images/features/features-light-01.png",
-    imageDark: "/images/features/features-dark-01.svg",
-  },
+    imageDark: "/images/features/features-dark-01.svg"
+  }
 ];
 
 export default featuresTabData;
